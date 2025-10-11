904465a2a699ceed6749d49e02fda21f1a6cccfd
6C656C65 <73671374+6C656C65@users.noreply.github.com>
2025-10-11 22:32:26 +0200

    Add pre-commit-config.yaml

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 import os
 
+
 def get_version():
     base_dir = os.path.dirname(__file__)
     version_path = os.path.join(base_dir, "pyproxy", "__init__.py")
@@ -8,5 +9,6 @@ def get_version():
     with open(version_path, "r") as f:
         exec(f.read(), version_ns)
     return version_ns["__version__"]
+
 
 setup(version=get_version())
